93cafcbda9f082a4de77b401d2030e164f22c5d7
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2025-05-10 11:34:55 +0200

    is -march=x86-64-v3 sufficient to support __builtin_addcl?

--- ci/perf_test.sh
+++ ci/perf_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export CORE_MATH_PERF_MODE=rdtsc CFLAGS="-march=x86-64-v2 -I/usr/local/include" LDFLAGS="-L/usr/local/lib"
+export CORE_MATH_PERF_MODE=rdtsc CFLAGS="-march=x86-64-v3 -I/usr/local/include" LDFLAGS="-L/usr/local/lib"
 ./perf.sh expf
 ./perf.sh exp
 ./perf.sh expl

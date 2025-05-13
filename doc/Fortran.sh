65c91f9891e79eeb4dd157960a184cb8e1a933d6
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2025-05-13 09:44:22 +0200

    applied patch from Vedran Novaković <venovako@gmail.com>
    (see https://sympa.inria.fr/sympa/arc/core-math/2025-05/msg00001.html)

--- doc/Fortran.sh
+++ doc/Fortran.sh
@@ -112,7 +112,7 @@ then
 fi
 # Please uncomment the lines below if your Fortran compiler supports the REAL128 kind.
 # echo "! quad unary functions"
-# for I in rsqrt
+# for I in rsqrt sqrt
 # do
 # 	echo "interface"
 # 	echo "   pure function cr_${I}q(x) bind(c,name='cr_${I}q')"

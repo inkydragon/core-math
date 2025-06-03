e057364be929f46b5f2728ff361dfdf068983488
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2025-06-03 15:06:32 +0200

    dummy exp16 function since GNU libc does not provide it yet

--- src/binary16/exp/expf16.c
+++ src/binary16/exp/expf16.c
@@ -1,6 +1,6 @@
 /* Correctly-rounded natural exponential function for binary32 value.
 
-Copyright (c) 2023-2025 Alexei Sibidanov.
+Copyright (c) 2025 Maxence Ponsardin.
 
 This file is part of the CORE-MATH project
 (https://core-math.gitlabpages.inria.fr/).
@@ -25,6 +25,7 @@ SOFTWARE.
 */
 
 #include <stdint.h>
+#include <math.h>
 
 // Warning: clang also defines __GNUC__
 #if defined(__GNUC__) && !defined(__clang__)
@@ -39,4 +40,9 @@ typedef union {double f; uint64_t u;} b64u64_u;
 
 _Float16 cr_expf16(_Float16 x){
   return x;
+}
+
+// dummy function since GNU libc does not provide it
+_Float16 expf16 (_Float16 x) {
+  return (_Float16) expf ((float) x);
 }

94a5fb32dd7781075d828338792f9c26e13afc84
Maxence Ponsardin <maxence.ponsardin@inria.fr>
2025-06-05 15:14:20 +0200

    Brought some modifications on used functions

--- src/binary16/exp/expf16.c
+++ src/binary16/exp/expf16.c
@@ -9,7 +9,7 @@ Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
+copies of the Software, and to permit persons to whom the Software isref=0x1.02p-15
 furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all
@@ -38,10 +38,11 @@ typedef union {_Float16 f; uint16_t u;} b16u16_u;
 typedef union {float f; uint32_t u;} b32u32_u;
 
 _Float16 cr_expf16(_Float16 x){
-	_Float16 x0 = -0x1.154p+4; // smallest _Float16 such that exp(x0-) < MIN_FLOAT16 / 2 <= exp(x0)
+	// _Float16 x0 = -0x1.154p4; // smallest _Float16 such that exp(x0-) < MIN_FLOAT16 / 2 <= exp(x0)
+	uint16_t x0 = 0xcc55; // binaryrepresentation of x0 in order to compare uint16_t rather than flt16
  	_Float16 x1 = 0x1.62bp3; // largest _Float16 such that exp(x1) <= MAX_FLOAT16 < exp(x1+)
 	static const float tb[] = // tabulate value of exp(i/2^6) for i in [-2^6, 2^6], size(tb) = 2^7 + 1
-		{0x5.e2d59p-4, 0x5.fa9038p-4, 0x6.12aa9p-4, 0x6.2b261p-4, 0x6.44044p-4, 0x6.5d46b8p-4, 0x6.76efp-4, 0x6.90feb8p-4, 
+		{0x5.e2d59p-4, 0x5.fa9038p-4, 0x6.12aa9p-4, 0x6.2b261p-4, 0x6.44044p-4, 0x6.5d46b8p-4, 0x6.76efp-4, 0x6.90feb8p-4,
 		 0x6.ab778p-4, 0x6.c65bp-4, 0x6.e1aae8p-4, 0x6.fd68fp-4, 0x7.1996c8p-4, 0x7.363638p-4, 0x7.53491p-4, 0x7.70d12p-4,
 		 0x7.8ed038p-4, 0x7.ad484p-4, 0x7.cc3b2p-4, 0x7.ebaacp-4, 0x8.0b992p-4, 0x8.2c083p-4, 0x8.4cfa1p-4, 0x8.6e70cp-4,
 		 0x8.906e5p-4, 0x8.b2f4ep-4, 0x8.d606bp-4, 0x8.f9a5dp-4, 0x9.1dd4ap-4, 0x9.42954p-4, 0x9.67eap-4, 0x9.8dd55p-4,
@@ -58,22 +59,26 @@ _Float16 cr_expf16(_Float16 x){
 		 0x2.1df3b8p+0, 0x2.267c8cp+0, 0x2.2f27c8p+0, 0x2.37f5f8p+0, 0x2.40e7a8p+0, 0x2.49fd64p+0, 0x2.5337c4p+0, 0x2.5c9754p+0,
 		 0x2.661cbp+0, 0x2.6fc87p+0, 0x2.799b28p+0, 0x2.83957cp+0, 0x2.8db808p+0, 0x2.980374p+0, 0x2.a2785cp+0, 0x2.ad177p+0, 0x2.b7e15p+0};
 
-	if (x < x0) return (_Float16) 0x1p-25f;
+	b16u16_u v = {.f = x};
+	if (v.u > x0) return (_Float16) 0x1p-25f;
 	else if (x > x1) return (_Float16) 0x1.ffcp15f + 0x1p4f; 
 	else {
-		float log2 = 0x1.62e430p-1;
-		int k = __builtin_roundeven((float) x / log2); 
-		float xp = (float) x - k * log2; // xp is a float such that |xp| is minimal and x = klog(2) + xp
+		float minus_log2 = -0x1.62e430p-1;
+		float inv_log2 = 0x1.715476p0f;
+		float k = __builtin_roundevenf(inv_log2 * x); 
+		float xp = __builtin_fmaf(k, minus_log2, x); // xp is a float such that |xp| is minimal and x = klog(2) + xp
 		int i = 0x40 * xp;
 		float xpp = xp - (float) i / 0x40; // x = klog(2) + i/2^6 + xpp
 																		   // So, exp(x) = 2^k * exp(i/2^6) * exp(xpp)
 
 		// result
 		xpp = 1.0 + xpp * (1 + xpp * (0.5 + xpp * (0x1.555644p-3 + xpp * 0x1.555bep-5)));
-		if (k >= 0) return (_Float16) (xpp * tb[i + (1<<6)] * (1 << k));
-		else return (_Float16) (xpp * tb[i + (1<<6)] / (1 << -k));
+		if (k >= 0) return (_Float16) (xpp * tb[i + (1<<6)] * (1 << (int)k));
+		else return (_Float16) (xpp * tb[i + (1<<6)] / (1 << (int) -k));
 	}
 }
+
+
 
 // dummy function since GNU libc does not provide it
 _Float16 expf16 (_Float16 x) {

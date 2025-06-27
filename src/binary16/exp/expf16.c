b5c3370ce06312eb87e0509c6d77c08084613603
Maxence Ponsardin <maxence.ponsardin@inria.fr>
2025-06-27 09:37:45 +0200

    Added better if in order to gain some time

--- src/binary16/exp/expf16.c
+++ src/binary16/exp/expf16.c
@@ -38,8 +38,8 @@ typedef union {_Float16 f; uint16_t u;} b16u16_u;
 typedef union {float f; uint32_t u;} b32u32_u;
 
 _Float16 cr_expf16(_Float16 x){
-	static const uint16_t u0 = 0xcc55; // binary representation of x0=-0x1.154p+4 in order to compare uint16_t rather than flt16
- 	static const _Float16 x1 = 0x1.62cp3; // largest _Float16 such that exp(x1) <= MAX_FLOAT16 < exp(x1+)
+	static const b16u16_u x0 = {.f = -0x1.154p+4f}; // smallest _Float16 such that exp(x0-) < MIN_SUBNORMALIZE <= exp(x0)
+ 	static const b16u16_u x1 = {.f = 0x1.62cp3f}; // largest _Float16 such that exp(x1) <= MAX_FLOAT16 < exp(x1+)
 	static const float tb[] = // tabulate value of exp(log(2)*i/2^6) for i in [0, 63]
 		{0x1p+0f, 0x1.02c9a2p+0f, 0x1.059b0cp+0f, 0x1.087452p+0f,  
 		 0x1.0b5586p+0f, 0x1.0e3ec4p+0f, 0x1.11301ep+0f, 0x1.1429aap+0f,  
@@ -57,29 +57,28 @@ _Float16 cr_expf16(_Float16 x){
 		 0x1.c199bep+0f, 0x1.c67f14p+0f, 0x1.cb720ep+0f, 0x1.d072d6p+0f,  
 		 0x1.d58190p+0f, 0x1.da9e6p+0f, 0x1.dfc974p+0f, 0x1.e502eep+0f,  
 		 0x1.ea4afap+0f, 0x1.efa1cp+0f, 0x1.f50766p+0f, 0x1.fa7c18p+0f};
-
 	b16u16_u v = {.f = x};
-	if ((v.u & 0x7c00) == 0x7c00) { // if x is nan or x is inf
-		if (v.u == 0xfc00) return 0x0p0;
-		else return x + x;
+	if ((v.u & 0x7fff) > 0x498b) { // in this case, we have x > min(x0, x1) in abs value
+		if ((v.u & 0x7c00) == 0x7c00) { // if x is nan or x is inf
+			if (v.u == 0xfc00) return 0x0p0;
+			else return x + x;
+		}
+		else if (v.u > x0.u) return 0x1p-25f; // x smaller than x0
+		else if (v.f > x1.f) return 0x1.ffcp15f + 0x1p5f; // x greater than x1
 	}
-	else if (v.u > u0) return 0x1p-25f; // x < -0x1.aap+25
-	else if (x > x1) return 0x1.ffcp15f + 0x1p5f;
-	else {
-		float xf = x; // exact conversion from _Float16 to float
-		static const float sixtyfour_over_log2 = 0x1.715476p+6f;
-		static const float minus_log2_over_sixtyfour = -0x1.62e430p-7f;
-		float j = __builtin_roundevenf(sixtyfour_over_log2 * xf);
-		uint32_t jint = j;
-		int i = jint & 0x3f;
-		float xp = __builtin_fmaf(minus_log2_over_sixtyfour, j, xf);
-		// xf = j*log(2)/64 + xp = (j>>6)*log(2) + log(2)*i/64 + xp
-		// so exp(xf) = 2^(j>>6) * exp(log(2)*i/64) * exp(xp)
-    xp = __builtin_fmaf(__builtin_fmaf(0.5f, xp, 1.0f), xp, 1.0f);
-		b32u32_u w = {.f = xp * tb[i]};
-		w.u += (jint >> 6) * (1l << 23);
-		return w.f; // conversion float -> _Float16 (with rounding)
-	}
+	float xf = x; // exact conversion from _Float16 to float
+	static const float sixtyfour_over_log2 = 0x1.715476p+6f;
+	static const float minus_log2_over_sixtyfour = -0x1.62e430p-7f;
+	float j = __builtin_roundevenf(sixtyfour_over_log2 * xf);
+	uint32_t jint = j;
+	int i = jint & 0x3f;
+	float xp = __builtin_fmaf(minus_log2_over_sixtyfour, j, xf);
+	// xf = j*log(2)/64 + xp = (j>>6)*log(2) + log(2)*i/64 + xp
+	// so exp(xf) = 2^(j>>6) * exp(log(2)*i/64) * exp(xp)
+	xp = __builtin_fmaf(__builtin_fmaf(0.5f, xp, 1.0f), xp, 1.0f);
+	b32u32_u w = {.f = xp * tb[i]};
+	w.u += (jint >> 6) * (1l << 23);
+	return w.f; // conversion float -> _Float16 (with rounding)
 }
 
 // dummy function since GNU libc does not provide it

488cc47295378aa333dd223079bce90da78a0cbe
Maxence Ponsardin <maxence.ponsardin@inria.fr>
2025-07-02 17:23:08 +0200

    [hypotf16] Fixed inexact exception

--- src/binary16/hypot/hypotf16.c
+++ src/binary16/hypot/hypotf16.c
@@ -43,23 +43,25 @@ _Float16 cr_hypotf16(_Float16 x, _Float16 y){
 	b64u64_u ty = {.f = y};
 	double ret = sqrt(tx.f * tx.f + ty.f * ty.f);
 #ifdef CORE_MATH_SUPPORT_ERRNO
-	if (0x1p0f + 0x1p-25f == 0x1p0f - 0x1p-25f) { // check if rounding mode is rndn
-		if (ret < 0x1.ffep-15 && (double) (_Float16) ret != ret) // underflow and inex
-			errno = ERANGE;
-		if (ret >= 0x1.ffep+15)
-			errno = ERANGE;
-	}
-	if (0x1p0f + 0x1.8p-24f == 0x1p0f) { // check if rounding mode is rndz/rndd
-		if (ret < 0x1p-14 && (double) (_Float16) ret != ret) // underflow and inex
-			errno = ERANGE;
-		if (ret > 0x1p+16)
-			errno = ERANGE;
-	}
-	if (0x1.000002p0f - 0x1.8p-24f == 0x1.000002p0f) { // check if rounding mode is rndu
-		if (ret < 0x1.ffcp-15 && (double) (_Float16) ret != ret) // underflow and inex
-			errno = ERANGE;
-		if (ret > 0x1.ffcp+15)
-			errno = ERANGE;
+	if ((tx.u << 1) >> 53 < 0x7ff && (ty.u << 1) >> 53 < 0x7ff && x != 0x0p0 && y != 0x0p0) {
+		if (0x1p0f + 0x1p-25f == 0x1p0f - 0x1p-25f) { // check if rounding mode is rndn
+			if (ret < 0x1.ffep-15 && (double) (_Float16) ret != ret) // underflow and inex
+				errno = ERANGE;
+			if (ret >= 0x1.ffep+15)
+				errno = ERANGE;
+		}
+		if (0x1p0f + 0x1.8p-24f == 0x1p0f) { // check if rounding mode is rndz/rndd
+			if (ret < 0x1p-14 && (double) (_Float16) ret != ret) // underflow and inex
+				errno = ERANGE;
+			if (ret > 0x1p+16)
+				errno = ERANGE;
+		}
+		if (0x1.000002p0f - 0x1.8p-24f == 0x1.000002p0f) { // check if rounding mode is rndu
+			if (ret < 0x1.ffcp-15 && (double) (_Float16) ret != ret) // underflow and inex
+				errno = ERANGE;
+			if (ret > 0x1.ffcp+15)
+				errno = ERANGE;
+		}
 	}
 #endif
 	return ret; 

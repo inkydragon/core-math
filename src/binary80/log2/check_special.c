c06dc17da0db6ca397f94b5a5e51dcab242b50a0
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2025-08-25 09:08:52 +0200

    [log2l] avoid compiler warning

--- src/binary80/log2/check_special.c
+++ src/binary80/log2/check_special.c
@@ -109,7 +109,9 @@ check (long double x)
   fesetround (rnd1[rnd]);
   feclearexcept (FE_INEXACT);
   long double y2 = cr_log2l (x);
+#ifdef CORE_MATH_CHECK_INEXACT
   int inex2 = fetestexcept (FE_INEXACT);
+#endif
   if (!is_equal (y1, y2))
   {
     printf ("FAIL x=%La ref=%La z=%La\n", x, y1, y2);

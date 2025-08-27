baa28ea1c273f8136c91eece7168e535c22124ff
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2025-08-27 15:38:08 +0200

    added sincosf16

--- src/generic/sincos/random_under_test.h
+++ src/generic/sincos/random_under_test.h
@@ -3,5 +3,5 @@
 static inline TYPE_UNDER_TEST random_under_test (void)
 {
   /* sample in [-pi,pi] */
-  return 2 * pi * ((TYPE_UNDER_TEST) rand() / (TYPE_UNDER_TEST) RAND_MAX) - pi;
+  return 2 * pi * ((double) rand() / (double) RAND_MAX) - pi;
 }

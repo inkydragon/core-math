670b52bcef57d8f32a106c5fec39d06c274f707d
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2025-08-28 16:15:56 +0200

    added erff16

--- src/generic/erf/random_under_test.h
+++ src/generic/erf/random_under_test.h
@@ -1,5 +1,5 @@
 static inline TYPE_UNDER_TEST random_under_test (void)
 {
   /* sample in [-4.16,4.16] */
-  return 8.32 * ((TYPE_UNDER_TEST) rand() / (TYPE_UNDER_TEST) RAND_MAX) - 4.16;
+  return 8.32 * ((double) rand() / (double) RAND_MAX) - 4.16;
 }

ee2addd35f416ed1efa05c59270af8ad56decc86
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2025-10-06 10:01:39 +0200

    [cbrt] fixed issue on gcc103 (underflow exception was reset)

--- src/binary64/cbrt/cbrt.c
+++ src/binary64/cbrt/cbrt.c
@@ -80,7 +80,7 @@ static inline int get_rounding_mode (fexcept_t *flagp)
 {
   /* Warning: on __aarch64__ (for example cfarm103), FE_UPWARD=0x400000
      instead of 0x800. */
-#if defined(__x86_64__) || defined(__aarch64__) ||  defined(__arm64__) || defined(_M_ARM64)
+#if defined(__x86_64__) ||  defined(__arm64__) || defined(_M_ARM64)
   *flagp = _mm_getcsr ();
   return ((*flagp)>>13) & 3;
 #else
@@ -101,7 +101,7 @@ static inline int get_rounding_mode (fexcept_t *flagp)
 
 static inline void set_flags (const fexcept_t *flagp)
 {
-#ifdef __x86_64__
+#if defined(__x86_64__) ||  defined(__arm64__) || defined(_M_ARM64)
   _mm_setcsr (*flagp);
 #else
   fesetexceptflag (flagp, FE_ALL_EXCEPT);

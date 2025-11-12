3a628e1c37084633148ce0c5a799fe8dad0e0dfe
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2025-11-12 14:47:46 +0100

    Revert "removed useless _GNU_SOURCE, since now exp10f is in C23"
    This reverts commit a6b9b0ac317a83982fae066067de8d2dea72d419.
    
    Reason: CI slaves are not yet fully C23.

--- src/binary16/exp10m1/exp10m1f16.c
+++ src/binary16/exp10m1/exp10m1f16.c
@@ -24,6 +24,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+#define _GNU_SOURCE // needed to define expf10
+
 #include <stdint.h>
 #include <errno.h>
 #include <math.h> // only used during performance tests

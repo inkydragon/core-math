702a447ebcbb78525965bde6cd64d3cd082899f8
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2025-11-24 16:40:25 +0100

    [atanh] allow XMAX for performance tests
    For example to exercise the branch |x| < 0.25:
    
    $ EXTRA_CFLAGS=-DXMAX=0.25 ./perf.sh atanh

--- src/generic/atanh/random_under_test.h
+++ src/generic/atanh/random_under_test.h
@@ -1,5 +1,9 @@
+#ifndef XMAX
+#define XMAX 1.0
+#endif
+
 static inline TYPE_UNDER_TEST random_under_test (void)
 {
-  /* sample in [-1,1] */
-  return 2 * ((double) rand() / (double) RAND_MAX) - 1;
+  /* sample in [-XMAX,XMAX] */
+  return 2.0 * XMAX * ((double) rand() / (double) RAND_MAX) - XMAX;
 }

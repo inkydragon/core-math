9b713295e20951fa50fb2fdc42efd3fe96d12cb5
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2025-12-10 07:34:23 +0100

    [acosh] restore parallel version of scan_consecutive

--- src/binary64/acosh/check_special.c
+++ src/binary64/acosh/check_special.c
@@ -46,6 +46,7 @@ SOFTWARE.
 #include <getopt.h>
 #include <errno.h>
 #include <mpfr.h>
+#include <assert.h>
 #include "function_under_test.h"
 
 double cr_function_under_test(double);
@@ -58,6 +59,7 @@ int rnd1[] = { FE_TONEAREST, FE_TOWARDZERO, FE_UPWARD, FE_DOWNWARD };
 
 int rnd;
 int verbose = 0;
+unsigned long tested = 0;
 
 static inline double tfun(double x){
   return cr_function_under_test(x);
@@ -68,6 +70,22 @@ static inline double rfun(double x){
 }
 
 typedef union {double f; uint64_t u;} b64u64_u;
+
+static inline uint64_t
+asuint64 (double x)
+{
+  b64u64_u u;
+  u.f = x;
+  return u.u;
+}
+
+static double
+asfloat64 (uint64_t n)
+{
+  b64u64_u u;
+  u.u = n;
+  return u.f;
+}
 
 double rand_arg(double s, unsigned int *seed){
   int64_t r0,r1;
@@ -91,6 +109,8 @@ double rand_arg2(unsigned int *seed){
 
 static void check (double x){
   b64u64_u yr = {.f = rfun(x)}, yt = {.f = tfun(x)};
+#pragma omp atomic update
+  tested ++;
   if (yr.u != yt.u) {
     printf("test_fun and ref_fun differ for x=%a\n", x);
     printf("test_fun gives %a\n", yt.f);
@@ -136,41 +156,17 @@ static void call_random(unsigned int seed, int64_t n, double a, double b){
   }
 }
 
-double fasttwosum(double a, double b, double *e){
-  double t = a + b;
-  *e = (a-t)+b;
-  return t;
-}
-
-// get a double-double linear approximation of acosh(x)
+// put in h+l a double-double approximation of acosh(x)
 static void
-dd_acosh_p (double *c0h, double *c0l, double *c1h, double *c1l, double x){
+dd_acosh (double *h, double *l, double x)
+{
   mpfr_t t;
   mpfr_init2 (t, 107);
-
   mpfr_set_d (t, x, MPFR_RNDN);
   mpfr_acosh (t, t, MPFR_RNDN);
-  *c0h = mpfr_get_d (t, MPFR_RNDN);
-  mpfr_sub_d (t, t, *c0h, MPFR_RNDN);
-  *c0l = mpfr_get_d (t, MPFR_RNDN);
-#if 1
-  mpfr_set_d (t, x, MPFR_RNDN);
-  mpfr_sqr (t, t, MPFR_RNDN);
-  mpfr_sub_d (t, t, 1.0, MPFR_RNDN);
-  mpfr_rec_sqrt(t, t, MPFR_RNDN);
-  *c1h = mpfr_get_d (t, MPFR_RNDN);
-  mpfr_sub_d (t, t, *c1h, MPFR_RNDN);
-  *c1l = mpfr_get_d (t, MPFR_RNDN);
-#else
-  double exm1, xm1 = fasttwosum(x,-1.0, &exm1);
-  double exp1, xp1 = fasttwosum(x, 1.0, &exp1);
-  double x2m1 = xm1*xp1, ex2m1 = exm1*xp1 + exp1*xm1 + fma(xm1,xp1, -x2m1);
-  double sx2m1 = sqrt(x2m1), d = 1.0 / sx2m1, esx2m1 = (ex2m1 - fma(sx2m1, sx2m1, -x2m1))*0.5*d;
-  double ed = (fma(d,-sx2m1, 1.0) - (esx2m1*d)*(1.0-(esx2m1*d)))*d;
-  *c1h = d;
-  *c1l = ed;
-#endif
-
+  *h = mpfr_get_d (t, MPFR_RNDN);
+  mpfr_sub_d (t, t, *h, MPFR_RNDN);
+  *l = mpfr_get_d (t, MPFR_RNDN);
   mpfr_clear (t);
 }
 
@@ -178,62 +174,45 @@ static void scan_consecutive(int64_t n, double x){
   ref_init();
   ref_fesetround(rnd);
   fesetround(rnd1[rnd]);
-  int64_t count = 0, reportstride = 1000l*1000l*1000l, nreport = reportstride;
+  if (n < 0) {
+    n = -n;
+    x = asfloat64 (asuint64 (x) - n);
+  }
+  int64_t n0 = n;
   while (n) {
-    /* acosh(x+h) ~= (c0h+c0l) + (c1h+c1l)*h + c2*h^2 + c3*h^3 + c4*h^4 local Taylor expansion*/
-    double c0h, c0l, c1h, c1l;
-    dd_acosh_p (&c0h, &c0l, &c1h, &c1l, x);
-    double t2 = c1h*c1h, t3 = t2*c1h, t4 = t2*t2;
-    double c2 = t3*x*-0.5;
-    double c3 = t2*t3*(2*x*x+1)/6;
-    double c4 = t4*t3*x*(2*x*x+3)*-0.125;
-
-    int e; frexp (x, &e);
+    double h, l, d, dd;
+    dd_acosh (&h, &l, x);
+    d = 1.0 / sqrt (x * x - 1.0); // derivative of acosh(x)
+    dd = d * fabs (x) / (x * x - 1.0); // absolute value of 2nd derivative
+    int e;
+    frexp (x, &e);
     /* 2^(e-1) <= |x| < 2^e thus ulp(x) = 2^(e-53) */
-    double h = ldexp(1.0, e - 53);
-
-    // c4*(jmax*h)^4 < 0x1p-100*c0 -- fourth order term is less then 0x1p-100 of the function value
-    int64_t jmax = 0x1p-25*sqrt(sqrt(fabs(c0h/c4)))/h;
+    d = ldexp (d, e - 53); // multiply d by ulp(x)
+    dd = ldexp (dd, 2 * (e - 53)); // multiply dd by ulp(x)^2
+    /* we want j^2*dd < 2^-11 ulp(h) so that the 2nd-order term
+       produces an error bounded by 2^-11 ulp(h), to that MPFR
+       will be called with probability about 2^-11.
+       Thus approximately j^2*dd < 2^-64 h,
+       or j < 2^-32 sqrt(h/dd) */
+    int64_t jmax = 0x1p-32 * sqrt (h / dd);
     if (jmax > n) jmax = n; // cap to n
-    double nbnd = ldexp(1.0,e);//next binade boundary
-    if (h*jmax + x > nbnd){ // do not cross the binade boundary with ulp unchanged
-      jmax = (nbnd - x)/h;
-    }
-    const int64_t ks = 256;
-    double fe, ref[ks];
-    for(int64_t j=0;j<jmax;){
-      double xl = j*h, fl, fh = fasttwosum(c0h, c1h*xl, &fl);
-      fl += (((c4*xl + c3)*xl + c2)*xl + c1l)*xl + c0l;
-      fh = fasttwosum(fh, fl, &fl);
-      double d1l = ((((((4*j + 6)*j + 4)*j + 1)*(c4*h) + c3*((j + 1)*3*j + 1))*h + c2*(2*j+1))*h + c1l)*h, d1h = c1h*h;
-      double d2 = 2*((c4*h*((6*j + 12)*j + 7) + c3*(3*j + 3))*h + c2)*h*h;
-      int kmax = ks;
-      if(jmax-j<ks) kmax = jmax-j;
-      for(int k=0;k<kmax;k++){ // fill first an array of almost correctly rounded results
-	ref[k] = fh + fl;
-	fh = fasttwosum(fh, d1h, &fe); fl += fe + d1l;
-	d1l += d2;
-      }
-      j += kmax;
-#if 0
-      // control of accuracy -- with current options and x=0x1.08p0 the accuracy is ~0x1p-94
-      double p0h, p0l, p1h, p1l;
-      dd_acosh_p (&p0h, &p0l, &p1h, &p1l, x + h*j);
-      printf("%ld %a\n", j, (fh-p0h)+(fl-p0l));
-#else
-      for(int k=0;k<kmax;k++){
-	if (__builtin_expect(tfun(x) != ref[k],0)) // expensive test
-	  check(x);
-	x += h;
-      }
+    assert (jmax > 0); // ensure progress
+#if (defined(_OPENMP) && !defined(CORE_MATH_NO_OPENMP))
+#pragma omp parallel for
 #endif
+    for(int64_t j=0;j<jmax;j++){
+      b64u64_u v = {.f = x};
+      v.u += j;
+      double t = tfun (v.f);
+      // acosh(x+j*u) is approximated by h + l + j*d
+      double w = h + __builtin_fma (j, d, l);
+      if (t != w) // expensive test
+        check(v.f);
     }
     n -= jmax;
-    if( (count += jmax) > nreport){
-      nreport += reportstride;
-      printf("n_tested = %ld x_current = %a\n", count, x);
-    }
+    x += jmax * ldexp (1.0, e - 53);
   }
+  printf ("checked %lu values, expensive checks %lu\n", n0, tested);
 }
 
 static void check_val(double x){

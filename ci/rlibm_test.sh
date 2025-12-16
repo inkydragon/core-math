d9b055051ac62780fb3edeb9cac50c6059c852ff
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2025-12-16 03:25:24 +0100

    [rlibm test] test all current RLIBM functions

--- ci/rlibm_test.sh
+++ ci/rlibm_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-for f in log2; do
-   gcc -DSTR=f -O3 -march=native ci/rlibm_test.c -lmpfr -lm /tmp/The-RLIBM-Project/libm/rlibm.a -fopenmp
+# updated 16 Dec 2025 (revision be69161 of RLIBM)
+for f in log10 log2 log exp10 exp2 exp cosh sinh cospi sinpi sin cos tan atan asin acos; do
+   echo Testing $f
+   gcc -DSTR=$f -O3 -march=native ci/rlibm_test.c -lmpfr -lm /tmp/The-RLIBM-Project/libm/rlibm.a -fopenmp
    ./a.out
 done

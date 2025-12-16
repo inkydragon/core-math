d2e751293c550e360a427c7a54fcbdbcbde05622
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2025-12-16 10:36:31 +0100

    better balance load between 2 RLIBM tests

--- ci/rlibm_test1.sh
+++ ci/rlibm_test1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # updated 16 Dec 2025 (revision be69161 of RLIBM)
-for f in log10 log2 log exp10 exp2 exp cosh sinh cospi sinpi sin cos; do
+for f in log10 log2 log exp10 exp2 exp cosh sinh; do
    echo Testing $f
    gcc -DSTR=$f -O3 -march=native ci/rlibm_test.c -lmpfr -lm /tmp/The-RLIBM-Project/libm/rlibm.a -fopenmp
    ./a.out
--- ci/rlibm_test2.sh
+++ ci/rlibm_test2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # updated 16 Dec 2025 (revision be69161 of RLIBM)
-for f in tan atan asin acos; do
+for f in cospi sinpi sin cos tan atan asin acos; do
    echo Testing $f
    gcc -DSTR=$f -O3 -march=native ci/rlibm_test.c -lmpfr -lm /tmp/The-RLIBM-Project/libm/rlibm.a -fopenmp
    ./a.out
